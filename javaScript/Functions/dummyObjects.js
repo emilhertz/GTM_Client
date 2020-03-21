2e65d603aebba2bce6899f3b088d0d4bc26d64e3
emilhertz <emhe19ae@student.cbs.dk>
2020-03-21 18:12:16 +0100

    Initial commit

---
+++ javaScript/Functions/dummyObjects.js
@@ -0,0 +1,23 @@
+//only relevant while localStorage is still used
+if (localStorage.getItem("storedListOfUsers") == null) {
+    let listOfUsers = [];
+    listOfUsers.push(new User(1,"Peter","Kanin","Rabbithole", "1234", "playboy@gmail.com"));
+    //admin-users
+    listOfUsers.push(new Admin(3, "Peter", "Legend", "Stallion", "Julemand", "hackerboi@peter.dk", "Peder Oxe"));
+
+    //listOfUsers stringifies, så de kan tilknyttes localStorage
+    let listOfUsersString = JSON.stringify(listOfUsers);
+    localStorage.setItem("storedListOfUsers", listOfUsersString);
+}
+
+if (localStorage.getItem("storedListOfRestaurants") == null) {
+    let listOfRestaurants = [];
+    listOfRestaurants.push(new Restaurant(1,"noma", new Address(1, "Refshalevej", 96,1432,"København K", "Danmark"), [],10,[],"Fin restaurant:)"));
+    listOfRestaurants.push(new Restaurant(2, "Geranium", new Address(2, "Per Henrik Lings Allé", 4, 2100, "København", "Danmark"),[],20,[],"Også en fin restaurant:)"));
+    listOfRestaurants.push(new Restaurant(3,"Sheik Shawarma Halal", new Address(3, "Nørrebrogade", 98, 2200,"København", "Danmark"),[],30,[],"MEGET fin restaurant:)"));
+
+    //listOfRestaurants stringifies, så de kan tilknyttes localStorage
+    let listOfUsersRestaurantsString = JSON.stringify(listOfRestaurants);
+    localStorage.setItem("storedListOfRestaurants", listOfUsersRestaurantsString);
+}
+
---
+++ javaScript/Functions/login.js
@@ -0,0 +1,59 @@
+//login metode
+let login = ()=> {
+    let listOfUsers = JSON.parse(localStorage.getItem("storedListOfUsers"));
+
+    let username = document.getElementById("loginUsername");
+    let password = document.getElementById("loginPassword");
+
+    //for-loop which uses operators to decide which user to sign in
+    for (let i=0; i < listOfUsers.length; i++) {
+        if (username.value === listOfUsers[i].userName && password.value === listOfUsers[i].password) {
+            //Stores signed in user in localStorage as a string
+            let signedIn = JSON.stringify(listOfUsers[i]);
+            localStorage.setItem("signedIn", signedIn);
+            if(listOfUsers[i].admin === true){
+                window.open("adminPage.html", "_self");
+                return;
+            } else {
+                //opens home.html
+                window.open("homePage.html", "_self");
+                return;
+            }
+        }
+    }
+    alert("Forkert brugernavn eller password :(")
+};
+
+//function which deletes the signed in user from localStorage and returns user to home-page
+let logout = ()=> {
+    localStorage.removeItem("signedIn");
+    window.open("homePage.html", "_self");
+};
+
+//asigns the signed in user to a global-varible
+let signedIn = JSON.parse(localStorage.getItem("signedIn"));
+
+//function is called on every html-page
+let dynamicNavBar = ()=> {
+    //decides what to display if user is signed in
+    if (signedIn !== null) {
+        document.getElementById("login").style.display = "none";
+        document.getElementById("logout").style.display = "block";
+        document.getElementById("reserve").style.display = "block";
+        document.getElementById("admin").style.display = "none";
+        document.getElementById("myRes").style.display = "block";
+        //decides what to display if user is admin
+        if (signedIn.admin === true) {
+            document.getElementById("admin").style.display = "block";
+            document.getElementById("reserve").style.display = "none";
+            document.getElementById("myRes").style.display = "none";
+        }
+        //decides what to display if no user is signed in
+    } else {
+        document.getElementById("login").style.display = "block";
+        document.getElementById("logout").style.display = "none";
+        document.getElementById("admin").style.display = "none";
+        document.getElementById("reserve").style.display = "none";
+        document.getElementById("myRes").style.display = "none";
+    }
+};
